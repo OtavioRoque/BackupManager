241a0e8e46c62a8405fac321ba6be4bb67eb2a42
Otávio Roque <roqueotavio5@gmail.com>
2025-09-03 15:52:06 -0300

    Adicionei o ClearableTextBox e ajustei o cabeçalho da MainWindow.xaml

---
+++ View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+#pragma warning disable CS8618
+
+namespace BackupManager.Views.UserControls
+{
+    public partial class ClearableTextBox : UserControl
+    {
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private string _placeHolder;
+
+        public string Placeholder
+        {
+            get { return _placeHolder; }
+            set { _placeHolder = value; }
+        }
+
+        private void btnClear_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text))
+                tbPlaceholder.Visibility = Visibility.Visible;
+            else
+                tbPlaceholder.Visibility = Visibility.Hidden;
+        }
+    }
+}

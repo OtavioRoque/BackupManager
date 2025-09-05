d80c004c2d20ec02b4d9512a7d106109a911c845
Otávio Roque <roqueotavio5@gmail.com>
2025-09-05 11:27:02 -0300

    Criei a pasta Model e o DatabaseModel.cs

---
+++ Model/DatabaseModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BackupManager.Model
+{
+    public class DatabaseModel : ObservableObject
+    {
+        private readonly string _name;
+        public string Name => _name;
+
+        private bool _isChecked;
+        public bool IsChecked
+        {
+            get => _isChecked;
+            set => SetProperty(ref _isChecked, value);
+        }
+
+        public DatabaseModel(string databaseName)
+        {
+            _name = databaseName;
+        }
+    }
+}

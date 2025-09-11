b4e1d6a45030960091033dcb975e4e13b128c64c
Otávio Roque <roqueotavio5@gmail.com>
2025-09-11 15:05:47 -0300

    Criei a classe DatabaseService para conter toda a lógica do shrink, backup e compact

---
+++ Helper/DatabaseService.cs
@@ -0,0 +1,20 @@
+﻿using BackupManager.Model;
+using System.IO;
+using System.IO.Compression;
+
+namespace BackupManager.Helper
+{
+    public class DatabaseService
+    {
+        private readonly string _destinationFolder;
+        private readonly bool _shrinkDatabase;
+        private readonly bool _backupDatabase;
+
+        public DatabaseService(string destinationFolder, bool shrinkDatabase, bool backupDatabase)
+        {
+            _destinationFolder = destinationFolder;
+            _shrinkDatabase = shrinkDatabase;
+            _backupDatabase = backupDatabase;
+        }
+    }
+}

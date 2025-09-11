113ed6068ccb1fa94eeea68f5cd487efba753694
Otávio Roque <roqueotavio5@gmail.com>
2025-09-11 15:17:51 -0300

    Finalizei a classe DatabaseService, deixando público apenas um método que vai fazer todo o processamento

--- Helper/DatabaseService.cs
+++ Helper/DatabaseService.cs
@@ -1,6 +1,7 @@
 ﻿using BackupManager.Model;
 using System.IO;
 using System.IO.Compression;
+using System.Windows;
 
 namespace BackupManager.Helper
 {
@@ -8,13 +9,89 @@ namespace BackupManager.Helper
     {
         private readonly string _destinationFolder;
         private readonly bool _shrinkDatabase;
-        private readonly bool _backupDatabase;
+        private readonly bool _compactDatabase;
 
         public DatabaseService(string destinationFolder, bool shrinkDatabase, bool backupDatabase)
         {
             _destinationFolder = destinationFolder;
             _shrinkDatabase = shrinkDatabase;
-            _backupDatabase = backupDatabase;
+            _compactDatabase = backupDatabase;
         }
+
+        #region Public methods
+
+        public bool ProcessDatabaseBackup(DatabaseModel database)
+        {
+            if (!DestinationFolderExists())
+                return false;
+
+            try
+            {
+                if (_shrinkDatabase)
+                    ShrinkDatabase(database);
+
+                BackupDatabase(database);
+
+                if (_compactDatabase)
+                    CompactDatabase(database);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void ShrinkDatabase(DatabaseModel database)
+        {
+            string sql = $"DBCC SHRINKDATABASE ({database.Name})";
+            DB.ExecuteNonQuery(sql);
+        }
+
+        private void BackupDatabase(DatabaseModel database)
+        {
+            string sql = @$"
+                BACKUP DATABASE [{database.Name}] 
+                TO DISK = N'{_destinationFolder}\{database.Name}.bak' 
+                WITH FORMAT, INIT, NAME = N'Backup de {database.Name}'";
+
+            DB.ExecuteNonQuery(sql);
+        }
+
+        private void CompactDatabase(DatabaseModel database)
+        {
+            string backupFile = @$"{_destinationFolder}\{database.Name}.bak";
+            string zipFile = @$"{_destinationFolder}\{database.Name}.zip";
+
+            using (var zipToOpen = new FileStream(zipFile, FileMode.Create))
+            using (var archive = new ZipArchive(zipToOpen, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(backupFile, Path.GetFileName(backupFile), CompressionLevel.Optimal);
+            }
+
+            DeleteBackupFile(backupFile);
+        }
+
+        private bool DestinationFolderExists()
+        {
+            return Directory.Exists(_destinationFolder);
+        }
+
+        /// <summary>
+        /// Apaga o .bak após a compactação pois não vai ser mais necessário.
+        /// </summary>
+        private void DeleteBackupFile(string backupFile)
+        {
+            if (File.Exists(backupFile))
+                File.Delete(backupFile);
+        }
+
+        #endregion
     }
 }

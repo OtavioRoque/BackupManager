2860a9840dd01568aeeed65ffd2c23e20f6ad5b3
Otávio Roque <roqueotavio5@gmail.com>
2025-09-17 11:48:53 -0300

    Criei a classe ProgressModel pra tratar o percentual da pbProgress

---
+++ Model/ProgressModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BackupManager.Model
+{
+    public class ProgressModel : ObservableObject
+    {
+        private string? _statusText;
+        public string StatusText
+        {
+            get => _statusText ?? string.Empty;
+            set => SetProperty(ref _statusText, value);
+        }
+
+        private double _percentage;
+        public double Percentage
+        {
+            get => _percentage;
+            set => SetProperty(ref _percentage, value);
+        }
+    }
+}

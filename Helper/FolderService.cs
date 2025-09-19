bc4818bfebcbbf816e7b4db693aae43be93d5b53
Otávio Roque <roqueotavio5@gmail.com>
2025-09-19 10:32:07 -0300

    Criei a classe FolderService pra segurar as demandas relacionadas manutenção de pastas

---
+++ Helper/FolderService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using System.IO;
+
+namespace BackupManager.Helper
+{
+    public static class FolderService
+    {
+        public static string SelectFolder(string title)
+        {
+            var dialog = new OpenFolderDialog()
+            {
+                Title = title
+            };
+
+            dialog.ShowDialog();
+
+            return dialog.FolderName;
+        }
+
+        public static bool FolderExists(string folderPath)
+        {
+            return Directory.Exists(folderPath);
+        }
+    }
+}

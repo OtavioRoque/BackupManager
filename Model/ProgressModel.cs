aeb393440b1ac3607852fda05821b88561e66cb4
Otávio Roque <roqueotavio5@gmail.com>
2025-09-19 08:25:48 -0300

    Aumentei o tamanho da pbProgress e apliquei otimizações do Toolkit no ProgressModel

--- Model/ProgressModel.cs
+++ Model/ProgressModel.cs
@@ -2,20 +2,12 @@
 
 namespace BackupManager.Model
 {
-    public class ProgressModel : ObservableObject
+    public partial class ProgressModel : ObservableObject
     {
+        [ObservableProperty]
         private double _percentage;
-        public double Percentage
-        {
-            get => _percentage;
-            set => SetProperty(ref _percentage, value);
-        }
 
-        private string? _statusText;
-        public string StatusText
-        {
-            get => _statusText ?? string.Empty;
-            set => SetProperty(ref _statusText, value);
-        }
+        [ObservableProperty]
+        private string _statusText = string.Empty;
     }
 }

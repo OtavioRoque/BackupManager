b12f7ad8e0659ce314dc137e5314e8a2914271e8
Otávio Roque <roqueotavio5@gmail.com>
2025-09-30 08:29:35 -0300

    Simplifiquei a propriedade Name tirando um backing field desnecessário

--- Model/DatabaseModel.cs
+++ Model/DatabaseModel.cs
@@ -4,15 +4,14 @@ namespace BackupManager.Model
 {
     public partial class DatabaseModel : ObservableObject
     {
-        private readonly string _name;
-        public string Name => _name;
+        public string Name { get; }
 
         [ObservableProperty]
         private bool _isChecked;
 
         public DatabaseModel(string databaseName)
         {
-            _name = databaseName;
+            Name = databaseName;
         }
     }
 }
